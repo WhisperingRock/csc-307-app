137d801665e7fc7d64e62ca1f3470da3a1377c5f
WhisperingRock <xpropellorheadx@gmail.com>
2026-04-06 15:23:58 -0700

    IE2 : step 2 complete

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,14 +5,56 @@ import express from "express"; //express = HTTP middleware dispatch
 const app = express(); 	// instance of express
 const port = 8000;	 	// listening port
 
+// hardcoding a table (for now)
+const users = 
+{
+	users_list: 
+	[
+		{
+			id: "xyz789",
+			name: "Charlie",
+			job: "Janitor",
+		},
+		{
+			id: "abc123",
+			name: "Mac",
+			job: "Bouncer",
+		},
+		{
+			id: "ppp222",
+			name: "Mac",
+			job: "Professor",
+		},
+		{
+			id: "yat999",
+			name: "Dee",
+			job: "Aspring actress",
+		},
+		{
+			id: "zap555",
+			name: "Dennis",
+			job: "Bartender",
+		},
+	],
+};
+
+
 app.use(express.json());// format incoming data as JSON
 
-// Respond with plain-text
-app.get("/", (req, res) =>   //first arg, "/" is URL pattern, other is callback func
+// User table at home/users
+app.get("/users", (req, res) =>   
+	{
+		res.send(users);
+	}
+);
+
+// HW at home directory
+app.get("/", (req, res) =>   
 	{
 		res.send("Hello World!");
 	}
 );
+
 
 // backend just listening
 app.listen(port, () => 
@@ -20,3 +62,5 @@ app.listen(port, () =>
 		console.log(`Example app listening at http://localhost:${port}`);
 	}
 );
+
+
